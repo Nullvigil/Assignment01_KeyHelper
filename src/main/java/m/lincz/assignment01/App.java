c462b2015d2d7511b31135bf4e3bdae4a5c4545c
Nullvigil <maxelantra@gmail.com>
2026-09-20 20:08:28 -0400

    Made an adequate script for making buttons and seems to work. The keyboard it makes is quite ugly so I'll think on how I could change it.

--- src/main/java/m/lincz/assignment01/App.java
+++ src/main/java/m/lincz/assignment01/App.java
@@ -3,6 +3,7 @@ package m.lincz.assignment01;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -29,28 +30,31 @@ public class App extends Application {
         //Number row
         String[] numbers = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
         for (int j = 0; j < numbers.length; j++) {
-            row++;
-            //TODO: Making the buttons
+            grid.add(createButton(numbers[j]), j, row);
         }
+        row++;
         
         //Letter row 1
         String[] letters1 = {"Q", "W", "E", "R", "T", "Y", "U", "i", "O", "P"};
-        for (int j = 0; j < numbers.length; j++) {
-            row++;
-            //TODO: Making the buttons
+        for (int j = 0; j < letters1.length; j++) {
+            grid.add(createButton(letters1[j]), j, row);
         }
+        row++;
+                
         //letter row 2
         String[] letters2 = {"A", "S", "D", "F", "G", "H", "J", "K", "L"};
-        for (int j = 0; j < numbers.length; j++) {
-            row++;
-            //TODO: Making the buttons
+        for (int j = 0; j < letters2.length; j++) {
+            grid.add(createButton(letters2[j]), j, row);
         }
+        row++;
+        
         //letter row 3
         String[] letters3 = {"Z", "X", "C", "V", "B", "N", "M"};
-        for (int j = 0; j < numbers.length; j++) {
-            row++;
-            //TODO: Making the buttons
+        for (int j = 0; j < letters3.length; j++) {
+            grid.add(createButton(letters3[j]), j, row);
         }
+        row++;
+                
         //misc row
         /*
         Button shiftBtn = ...
@@ -72,6 +76,14 @@ public class App extends Application {
         stage.setTitle("Type Helper");
         stage.setScene(scene);
         stage.show();
+    }
+    
+        
+    private Button createButton(String text) {
+        var btn = new Button(text);
+        btn.setPrefSize(40, 40);
+        btn.setOnAction(e -> System.out.println(text));
+        return btn;
     }
 
     public static void main(String[] args) {
