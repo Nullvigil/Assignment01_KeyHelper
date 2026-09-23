0dcbd2f64e34e2519f692f1b21b849fd64d42414
Nullvigil <maxelantra@gmail.com>
2026-09-23 14:51:43 -0400

    Added the other essential buttons and works with different sizes. I am deciding to probably remove the number row due to it being useless. TODO: work with shift to add modifiers to the letters.

--- src/main/java/m/lincz/assignment01/App.java
+++ src/main/java/m/lincz/assignment01/App.java
@@ -26,51 +26,55 @@ public class App extends Application {
         
         
         int row = 0;
-        
+        /**
         //Number row
         String[] numbers = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
         for (int j = 0; j < numbers.length; j++) {
-            grid.add(createButton(numbers[j]), j, row);
+            grid.add(createButton(numbers[j],""), j, row);
         }
         row++;
-        
+        **/
         //Letter row 1
         String[] letters1 = {"Q", "W", "E", "R", "T", "Y", "U", "i", "O", "P"};
         for (int j = 0; j < letters1.length; j++) {
-            grid.add(createButton(letters1[j]), j, row);
+            grid.add(createButton(letters1[j], ""), j, row);
         }
+        grid.add(createButton("<-", ""), 10, row);
         row++;
                 
         //letter row 2
         String[] letters2 = {"A", "S", "D", "F", "G", "H", "J", "K", "L"};
         for (int j = 0; j < letters2.length; j++) {
-            grid.add(createButton(letters2[j]), j, row);
+            grid.add(createButton(letters2[j], ""), j, row);
         }
+        grid.add(createButton("Enter", "Big"), 9, row, 2, 1);
         row++;
         
         //letter row 3
         String[] letters3 = {"Z", "X", "C", "V", "B", "N", "M"};
-        for (int j = 0; j < letters3.length; j++) {
-            grid.add(createButton(letters3[j]), j, row);
+        for (int j = 2; j < letters3.length + 2; j++) { //offset btns
+            grid.add(createButton(letters3[j - 2], ""), j, row);
         }
+        grid.add(createButton("Shift", "Big"), 0, row, 2, 1); 
+        grid.add(createButton(".", ""), 9, row);
+        grid.add(createButton(",", ""), 10, row);
         row++;
-                
+        
+        grid.add(createButton("___", "Space"), 2, row, 8, 1);
+        grid.add(createButton("?", ""), 9, row); //TODO: Combine , with ? when shift
+        grid.add(createButton("!", ""), 10, row ); // TODO: Combine . with ! when shift
+        
         //misc row
-        /*
-        Button shiftBtn = ...
+                
+
+                
+
+                
         
-        Button spaceBtn = ...
         
-        Button enterBtn = ...
         
-        Button dotBtn = ...
         
-        button commaBtn = ...
         
-        button questionBtn = ...
-        
-        button exclamBtn = ...
-        */
         
         var scene = new Scene(grid);
         stage.setTitle("Type Helper");
@@ -79,9 +83,15 @@ public class App extends Application {
     }
     
         
-    private Button createButton(String text) {
+    private Button createButton(String text, String type) {
         var btn = new Button(text);
-        btn.setPrefSize(40, 40);
+        if (type == "Big") {
+            btn.setPrefSize(85, 40);
+        }  else if (type == "Space") {
+            btn.setPrefSize(305, 40);
+        } else {
+            btn.setPrefSize(40, 40);
+        }
         btn.setOnAction(e -> System.out.println(text));
         return btn;
     }
@@ -89,5 +99,7 @@ public class App extends Application {
     public static void main(String[] args) {
         launch();
     }
+    
+    //FOR USING ComboBox comboBox.setOnAction(event - >
 
 }
